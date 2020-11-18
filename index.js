37fa28705410817f588b8f6a41f7e8d41b0a7929
hugodelgado <hugo.delgado1@outlook.com>
2020-11-18 12:45:29 -0600

    commit tree 59ab740628cb3b2b6c1c5f4b94c39604f6b789b1

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hourInString = time.split(":")
+  const parseHours = parseInt(hourInString);
+  // console.log(parseHours)
+
+  if (parseHours < 12) {
+    return displayMessage("Good Morning");
+  } else if (parseHours >= 12 && parseHours <= 17) {
+    return displayMessage("Good Afternoon");
+  } else {
+    return displayMessage("Good Evening");
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  const greeting = document.querySelector('#greeting')
+  greeting.innerText = message;
+  return message;
+}
